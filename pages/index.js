ac1deaf23fe39b4e4c2691c9338e9037dedb2795
Rifqi Fadhil <rifqife@gmail.com>
2022-09-03 06:29:09 +0700

    Update landing | adding questions

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-import { Col, Image, Row } from "react-bootstrap";
+// Components
 import Master from "../components/Master";
+
+// Dependencies
+import Link from "next/link";
+import { Col, Image, Row } from "react-bootstrap";
+import { motion } from "framer-motion";
 
 function Index() {
 	return (
@@ -11,10 +16,9 @@ function Index() {
 			/>
 			<Row className="justify-content-center bg-landing h-100">
 				<Col lg={6} className="position-relative" style={{ zIndex: 1 }}>
-					<h1 className="mt-3 text-center font-quick-sand-bold font-size-30">
-						Mainkan quiz Pristime
-						<br />
-						It's Pristine8.6+ Time versi Kamu!
+					<h1 className="text-center font-quick-sand-bold font-size-28">
+						Mainkan quiz Pristime <br /> It's Pristine8.6+ Time
+						versi Kamu!
 					</h1>
 					<Row className="justify-content-center">
 						<Col lg={4}>
@@ -45,9 +49,15 @@ function Index() {
 					</Row>
 					<Row className="justify-content-center">
 						<Col lg={5} className="d-grid">
-							<a className="rounded-pill bg-light-green border-white text-center font-quick-sand-bold font-size-18 py-1">
-								MAIN SEKARANG!
-							</a>
+							<Link href="/question?number=1">
+								<motion.div
+									className="rounded-pill bg-light-green border-white text-center font-quick-sand-bold font-size-18 py-1 cursor-pointer"
+									whileHover={{ scale: 1.1 }}
+									whileTap={{ scale: 1 }}
+								>
+									MAIN SEKARANG!
+								</motion.div>
+							</Link>
 						</Col>
 					</Row>
 				</Col>
