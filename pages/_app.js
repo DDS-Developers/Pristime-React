c98c522ea3b59bf8cd2aada19515048847b463e5
Rifqi Fadhil <rifqife@gmail.com>
2022-09-06 11:22:30 +0700

    Update GA

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,11 @@ import "../styles/globals.css";
 
 // Scripts
 import TagManager from "react-gtm-module";
-import ReactGA from "react-ga";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
 	useEffect(() => {
 		TagManager.initialize({ gtmId: "GTM-N4FBJD8" });
-		ReactGA.initialize("G-82VSCQ513F");
-		ReactGA.pageview(window.location.pathname + window.location.search);
 	}, []);
 
 	return <Component {...pageProps} />;
